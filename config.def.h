ecb3ecb89904b194c40665f1ba71d3835cad745b
Alec Davis <unlikelytitan@gmail.com>
2026-02-01 12:25:52 -0700

    added pam patch

--- config.def.h
+++ config.def.h
@@ -7,10 +7,14 @@ static const char *colorname[NUMCOLS] = {
 	[INPUT] =  "#005577",   /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
 	[CAPS] = "red",         /* CapsLock on */
+	[PAM] =    "#9400D3",   /* waiting for PAM */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* PAM service that's used for authentication */
+static const char* pam_service = "login";
 
 /* time in seconds before the monitor shuts down */
 static const int monitortime = 5;
